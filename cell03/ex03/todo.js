2a69790f5fae3ff5219c6ff5ac50a0f20c5a429c
thibaut1304 <thibaut.husser@free.fr>
2022-06-24 00:37:07 +0200

    cookie done

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,29 +1,54 @@
 
 var index = 0;
-var i = 0;
+
+function setCookie(cname,cvalue,exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function restoreCookies() {
+    if (!document.cookie)
+        return ;
+    const cookies = document.cookie.split(";");
+    for (const cookie of cookies) {
+        const value = cookie.split("=")
+        test(value[1]);
+    }
+}
 
 window.onload = () => {
 	index = 1;
+	// let x = document.cookie;
+	// console.log(x);
+	restoreCookies();
 }
 
 function remove() {
 	let text = "OK for remove ?\nOK or Cancel.";
 	if (confirm(text) == true) {
+		// console.log("test : " + this.getAttribute('to_do'));
+		let x = this.getAttribute('to_do');
+		document.cookie = x+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+		// console.log(document.cookie);
 		this.remove();
-	} 
+	}
 }
 
 function test(_text) {
 	let obj = document.getElementById('ft_list');
 	let div = document.createElement("div");
 	div.classList.add("to_do"); //Ajouter class
+	div.setAttribute("to_do", _text);
 	// div.onclick = function() {alert("Hey !");}
 	// div.setAttribute("onclick","alert('Hey !');");
 	div.addEventListener("click", remove);
-	div.textContent = _text + " " + ++i; // ajouter content in div !
+	div.textContent = _text; // ajouter content in div !
 	obj.insertBefore(div, obj.children[1]);
 	// obj.appendChild(div, ft_list);
-	// document.cookie = 
+	// document.cookie = _text;
+	setCookie(_text, _text, 30);
 }
 
 function load_window () {
@@ -34,18 +59,3 @@ function load_window () {
 	// document.getElementById("ft_list").innerHTML = text;
 }
 
-// function cssLayout() {
-//     document.getElementById("css").href = this.value;
-// }
-
-
-// function setCookie(){
-//     var date = new Date("Februari 10, 2013");
-//     var dateString = date.toGMTString();
-//     var cookieString = "Css=document.getElementById("css").href" + dateString;
-//     document.cookie = cookieString;
-// }
-
-// function getCookie(){
-//     alert(document.cookie);
-// }
